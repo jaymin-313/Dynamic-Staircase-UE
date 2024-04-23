8fbc51ee1f27831cca104fc836887c3064ca3997
Jaymin - ZTI <ZTI@DESKTOP-EUEIB0S>
2024-04-23 16:00:28 +0530

    Dynamic Staircase built

---
+++ Source/DynamicStaircase/DynamicStaircaseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DynamicStaircaseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICSTAIRCASE_API ADynamicStaircaseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
